894f13c39da4386108cbb790b76e192a6f053d77
The-Bowman <72958677+The-Bowman@users.noreply.github.com>
2020-11-12 22:07:16 -0700

    Add project files.

---
+++ CST117_OC08_console/Class1.cs
@@ -0,0 +1,94 @@
+﻿//Corrected by Dustin Johnson
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Lydia's code - find the errors!
+namespace CST117_IC08_console
+{
+    class Set
+    {
+        private List<int> elements;
+
+
+        public Set()
+        {
+            elements = new List<int>();
+        }
+
+        //adding a new add method to add elements
+        //to a Set that allows for duplicates
+        public void add(int val)
+        {
+            elements.Add(val);
+        }
+
+        public bool addElement(int val)
+        {
+            if (containsElement(val)) return false;
+            else
+            {
+                elements.Add(val);
+                return true;
+            }
+        }
+
+        private bool containsElement(int val)
+        {
+            //create a boolean variable to store 
+            //what happens in the loop, initialize
+            //to avoid compiler error 
+            bool result = false;
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (val == elements[i])
+                {
+                    result = true;
+                    //add break out statement if element is
+                    //contained there is no need to continue
+                    break;
+                }
+                else
+                    result = false;
+            }
+            //having this will always return false regardless 
+            //return false;
+            return result;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            foreach (int i in elements)
+            {
+                str += i + " ";
+            }
+            return str;
+        }
+
+        public void clearSet()
+        {
+            elements.Clear();
+        }
+
+        //This method should compare the elements
+        //of each Set, only adding what isn't already there
+        //public Set union(Set rhs)
+        public static Set union(Set a, Set b)
+        {
+            //add a new Set that is the union of two sets
+            Set unionList = new Set();
+          //  for (int i = 0; i < rhs.elements.Count; i++)
+          for (int i = 0; i < a.elements.Count; i++)
+            {
+                //this.addElement(rhs.elements[i]);
+                unionList.addElement(a.elements[i]);
+                unionList.addElement(b.elements[i]);
+            }
+            //this returns the wrong set
+            // return rhs;
+            return unionList;
+        }
+    }
+}
---
+++ CST117_OC08_console/Program.cs
@@ -0,0 +1,43 @@
+﻿//use for IC08
+//Lydia's code
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CST117_IC08_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //make some sets
+            Set A = new Set();
+            Set B = new Set();
+
+            //put some stuff in the sets
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                // A.addElement(r.Next(4));
+                // B.addElement(r.Next(12));
+                A.add(r.Next(4));
+                B.add(r.Next(12));
+            }
+
+            //display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            //The union method has changed, this line must change to accomodate
+            //the updated method call
+            //Console.WriteLine("A union B: " + A.union(B));
+            Console.WriteLine("A union B: " + Set.union(A, B));
+
+            //display original sets (should be unchanged)
+            Console.WriteLine("After union operation");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+        }
+    }
+}
